25b63720aa0d1786d53cc8b8afa8daac3ca2a4e4
AliHamza <alihamza@geo75.com>
2023-08-07 11:06:56 +0500

    initial setup

---
+++ src/Products/Products.js
@@ -0,0 +1,59 @@
+import iphone from  '../assests/products/1.png'
+import laptop from  '../assests/products/2.png'
+import camera from  '../assests/products/3.png'
+import shirt from  '../assests/products/4.png'
+import cpu from  '../assests/products/5.png'
+import whiteshirt from  '../assests/products/6.webp'
+import greenshirt from  '../assests/products/7.webp'
+import cap from  '../assests/products/8.webp'
+export const PRODUCTS=[
+    {   
+        pid:1,
+        name:'iPhone',
+        price:2200.0,
+        img:iphone
+    },
+    {   
+        pid:2,
+        name:'Laptop',
+        price:4300.0,
+        img:laptop
+    },
+    {   
+        pid:3,
+        name:'camera',
+        price:9200.0,
+        img:camera
+    },
+    {   
+        pid:4,
+        name:'shirt',
+        price:5200.0,
+        img:shirt
+    },
+    {   
+        pid:5,
+        name:'Cpu',
+        price:2200.0,
+        img:cpu
+    },
+    {   
+        pid:6,
+        name:'white shirt',
+        price:5400.0,
+        img:whiteshirt
+    },
+    {   
+        pid:7,
+        name:'Green Shirt',
+        price:2100.0,
+        img:greenshirt
+    },
+    
+    {   
+        pid:8,
+        name:'iPhone',
+        price:764.0,
+        img:cap
+    },
+]
